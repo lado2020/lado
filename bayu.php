c16d0dc4f40b3f84d46ebefc68b25004ef7f7084
lado2020 <62183677+lado2020@users.noreply.github.com>
2020-03-21 04:25:53 +0700

    Add files via upload

---
+++ bayu.php
@@ -0,0 +1,79 @@
+<?php
+
+error_reporting(0);
+include ("function.php");
+echo "\e            GOJEK VERSION 1.8.3              \n";
+echo "\e SCRIPT GOJEK AUTO CLAIM ALL PROMO BAYU DWI DIRGANTARA\n";
+echo "\n";
+nope:
+echo "\e[?] Masukkan Nomor HP Anda : ";
+$nope = trim(fgets(STDIN));
+$cek = cekno($nope);
+if ($cek == false)
+    {
+    echo "\e[x] Nomor Telah Terdaftar\n";
+			goto nope;
+    }
+  else
+    {
+echo "\e[!] Siapkan OTPmu\n";
+sleep(5);
+$register = register($nope);
+if ($register == false)
+    {
+    echo "\e[x] Failed Get OTP!\n";
+    }
+  else
+    {
+    otp:
+    echo "\e[!] Masukkan Kode Verifikasi (OTP) : ";
+    $otp = trim(fgets(STDIN));
+    $verif = verif($otp, $register);
+    if ($verif == false)
+        {
+        echo "\e[x] Kode Verifikasi Salah\n";
+        goto otp;
+        }
+      else
+        {
+	echo "\e[!] Trying to redeem Voucher : COBAGORIDEPAY !\n";
+        $claim = claim1($verif);
+        if ($claim == false){
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+			      sleep(3);
+            echo "\e[!] Trying to redeem Voucher : COBAGOFOOD090320A !\n";
+			      goto ride;
+            }else{
+                echo "\e[+] ".$claim."\n";
+				    sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAGOCARPAY !\n";
+                sleep(3);
+                goto ride;
+            }
+            ride:
+            $claim = ride($verif);
+            if ($claim == false){
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+			      sleep(3);
+            echo "\e[!] Trying to redeem Voucher :COBAINGORIDE !\n";
+            sleep(3);
+            }else{
+                echo "\e[+] ".$claim."\n";
+				    sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAGOFOOD090320A !\n";
+                sleep(3);
+                goto pengen;
+            }
+            pengen:
+            $claim = cekvocer($verif);
+            if ($claim == false ) {
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+           }
+		  else
+			{
+			echo $claim . "\n";
+			}
+		}
+	}
+}
+?>
